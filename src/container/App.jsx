a1c6a9a4608881ac100416019e37339cc32c73c3
marcelo miyoshi <miyoshi_marcelo@hotmail.com>
2021-11-19 17:32:50 -0300

    fix  commit

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/App.scss';
-import Banner from '../components/Banner/banner';
-import Header from '../components/Header/header';
-import Footer from '../components/Footer/footer';
-import Cards from '../components/Projetos/projeto';
+import Banner from '../components/Banner/banner.jsx';
+import Header from '../components/Header/header.jsx';
+import Footer from '../components/Footer/footer.jsx';
+import Cards from '../components/Projetos/projeto.jsx';
 
 function App() {
   return (
